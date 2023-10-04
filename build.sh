223f8b21ed23f0a1d19ff1d2beff50484519134b
Jamie <jdennis9@proton.me>
2023-10-04 14:29:15 +1300

    Comment out optional features in build.sh

--- build.sh
+++ build.sh
@@ -8,11 +8,11 @@ DEPS="$(pkg-config --cflags --libs wlroots) $(pkg-config --cflags --libs wayland
 SOURCES="capra.c xdg-shell-protocol.c"
 
 # Uncomment to enable XWayland
-XWAYLAND_CFLAGS="-DUSE_XWAYLAND=1"
-XWAYLAND_LIBS="-lxcb -lxcb-icccm"
+# XWAYLAND_CFLAGS="-DUSE_XWAYLAND=1"
+# XWAYLAND_LIBS="-lxcb -lxcb-icccm"
 
 # Uncomment to enable MPD support on status bar
-DEPS="$DEPS -DENABLE_MPD_STATUS=1 -lmpdclient"
+# DEPS="$DEPS -DENABLE_MPD_STATUS=1 -lmpdclient"
 
 set -x
 $CC $CFLAGS $XWAYLAND_CFLAGS $SOURCES $DEPS -I. -Wall -DWLR_USE_UNSTABLE -lm -linput $XWAYLAND_LIBS -o capra
